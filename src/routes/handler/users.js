ed91263b5afa9854cbe1b176b063f9e6c29eb3d7
Asya'ri <asyari818@gmail.com>
2023-11-14 17:16:19 +0700

    feat(auth): Get User by Id

--- src/routes/handler/users.js
+++ src/routes/handler/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const register = require('./users/register');
+const getUserById = require('./users/getUserById');
 
 router.post('/register', register);
+router.get('/:id', getUserById);
 
 module.exports = router;
