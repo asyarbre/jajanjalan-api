0d247b861432efc5aec5923d95e0ffec1cd50265
Asya'ri <asyari818@gmail.com>
2023-11-14 17:25:59 +0700

    feat(auth): update user

--- src/routes/handler/users.js
+++ src/routes/handler/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const register = require('./users/register');
 const getUserById = require('./users/getUserById');
+const update = require('./users/update');
 
 router.post('/register', register);
 router.get('/:id', getUserById);
+router.patch('/:id', update);
 
 module.exports = router;
