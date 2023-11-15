3f0718c65f70ed9558e524e2323bf7787c1b5d25
Asya'ri <asyari818@gmail.com>
2023-11-15 13:12:41 +0700

    feat(auth): login

--- src/routes/handler/users.js
+++ src/routes/handler/users.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const register = require('./users/register');
 const getUserById = require('./users/getUserById');
 const update = require('./users/update');
+const login = require('./users/login');
 
 router.post('/register', register);
 router.get('/:id', getUserById);
 router.patch('/:id', update);
+router.post('/login', login);
 
 module.exports = router;
