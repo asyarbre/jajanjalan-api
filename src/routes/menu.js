1c5c3efc90d74d17cc07be03b0e09b0d00b7a8ed
Asya'ri <asyari818@gmail.com>
2023-11-22 19:12:01 +0700

    feat(menu): get data by menu id

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const verifyToken = require("../middlewares/verifyToken");
 const permission = require("../middlewares/permission");
 const createMenu = require("./handler/menu/createMenu");
+const getMenuById = require("./handler/menu/getMenuById");
 
 router.post("/create", verifyToken, permission('penjual'), createMenu);
+router.get("/:id", getMenuById);
 
 module.exports = router;
