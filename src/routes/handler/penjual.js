beece9373c7fba75b8a6c4cb2b8339c6786c0c11
Asya'ri <asyari818@gmail.com>
2023-11-22 11:37:26 +0700

    feat(penjual): delete data penjual

--- src/routes/handler/penjual.js
+++ src/routes/handler/penjual.js
@@ -3,9 +3,11 @@ const verifyToken = require("../../middlewares/verifyToken");
 const permission = require("../../middlewares/permission");
 const createPenjual = require("./penjual/createPenjual");
 const getByPenjualId = require("./penjual/getByPenjualId");
+const deletePenjual = require("./penjual/deletePenjual");
 const router = express.Router();
 
 router.post("/create", verifyToken, permission('penjual'), createPenjual);
 router.get("/:id", getByPenjualId);
+router.delete("/:id", verifyToken, permission('penjual'), deletePenjual);
 
 module.exports = router;
