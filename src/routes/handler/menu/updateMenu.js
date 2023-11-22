53ee9c0e19f553eaa7a80ed4d860f4cffff663e9
Asya'ri <asyari818@gmail.com>
2023-11-22 20:07:05 +0700

    feat(menu): update data menu

---
+++ src/routes/handler/menu/updateMenu.js
@@ -0,0 +1,81 @@
+const prisma = require("../../../utils/db");
+const path = require("path");
+const storage = require("../../../utils/cloudStorage");
+
+const updateMenu = async (req, res) => {
+  try {
+    const { item, price, description } = req.body;
+    const image = req.file;
+    const userId = req.userData.id;
+    const menuId = parseInt(req.params.id);
+
+    const penjualId = await prisma.penjual.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    const menu = await prisma.menu.findUnique({
+      where: {
+        id: menuId,
+      },
+    });
+
+    if (!menu) {
+      return res.status(404).json({
+        error: "Menu not found",
+      });
+    }
+
+    if (menu.penjualId !== penjualId.id) {
+      return res.status(403).json({
+        error: "Forbidden",
+      });
+    }
+
+    const bucketName = "jajanjalan-storage";
+    const fileName = `${Date.now()}_${path.basename(image.originalname)}`;
+    const file = storage.bucket(bucketName).file(`images/${fileName}`);
+
+    const fileStream = file.createWriteStream({
+      metadata: {
+        contentType: image.mimetype,
+      },
+    });
+
+    fileStream.on("error", (err) => {
+      console.error("Error uploading image:", err);
+      res.status(500).json({ error: "Failed to upload image" });
+    });
+
+    fileStream.on("finish", async () => {
+      const imageUrl = `https://storage.googleapis.com/${bucketName}/images/${fileName}`;
+
+      const updatedMenu = await prisma.menu.update({
+        where: {
+          id: menuId,
+        },
+        data: {
+          item,
+          price: parseInt(price),
+          description,
+          image: imageUrl,
+        },
+      });
+
+      res.json({
+        message: "Menu updated",
+        data: updatedMenu,
+      });
+    });
+
+    fileStream.end(image.buffer);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+}
+
+module.exports = updateMenu;
--- src/routes/menu.js
+++ src/routes/menu.js
@@ -6,10 +6,12 @@ const createMenu = require("./handler/menu/createMenu");
 const getMenuById = require("./handler/menu/getMenuById");
 const getMenuByPenjualId = require("./handler/menu/getMenuByPenjualId");
 const deleteMenu = require("./handler/menu/deleteMenu");
+const updateMenu = require("./handler/menu/updateMenu");
 
 router.post("/create", verifyToken, permission('penjual'), createMenu);
 router.get("/:id", getMenuById);
 router.get("/penjual/:id", getMenuByPenjualId);
+router.patch("/:id", verifyToken, permission('penjual'), updateMenu);
 router.delete("/:id", verifyToken, permission('penjual'), deleteMenu);
 
 module.exports = router;
