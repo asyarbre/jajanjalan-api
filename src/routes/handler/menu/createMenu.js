0575f61b1d5b15a467be03b206141b64cac6bd83
Asya'ri <asyari818@gmail.com>
2023-11-22 19:47:48 +0700

    feat(menu): get all menu by penjual id

--- src/routes/handler/menu/createMenu.js
+++ src/routes/handler/menu/createMenu.js
@@ -2,22 +2,18 @@ const path = require("path");
 const prisma = require("../../../utils/db");
 const storage = require("../../../utils/cloudStorage");
 
-// Create menu handler
 const createMenu = async (req, res) => {
   try {
-    // Extract data from request body
     const { item, price, description } = req.body;
     const image = req.file;
     const userId = req.userData.id;
 
-    //find penjual id
     const penjualId = await prisma.penjual.findUnique({
       where: {
         userId,
       },
     });
     
-    // Upload image to Google Cloud Storage
     const bucketName = "jajanjalan-storage";
     const fileName = `${Date.now()}_${path.basename(image.originalname)}`;
     const file = storage.bucket(bucketName).file(`images/${fileName}`);
@@ -34,10 +30,8 @@ const createMenu = async (req, res) => {
     });
 
     fileStream.on("finish", async () => {
-      // Get the public URL of the uploaded image
       const imageUrl = `https://storage.googleapis.com/${bucketName}/images/${fileName}`;
 
-      // TODO: Save menu data to database
       const menu = await prisma.menu.create({
         data: {
           penjualId: penjualId.id,
--- src/routes/menu.js
+++ src/routes/menu.js
@@ -4,8 +4,11 @@ const verifyToken = require("../middlewares/verifyToken");
 const permission = require("../middlewares/permission");
 const createMenu = require("./handler/menu/createMenu");
 const getMenuById = require("./handler/menu/getMenuById");
+const getMenuByPenjualId = require("./handler/menu/getMenuByPenjualId");
 
 router.post("/create", verifyToken, permission('penjual'), createMenu);
 router.get("/:id", getMenuById);
+router.get("/penjual/:id", getMenuByPenjualId);
+
 
 module.exports = router;
