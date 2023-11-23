a95aafbb8ada0ea8795c7a3b196c186d3af14ecd
Asya'ri <asyari818@gmail.com>
2023-11-23 10:12:55 +0700

    feat(review): update review

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -4,10 +4,12 @@ const verifyToken = require('../middlewares/verifyToken');
 const permission = require('../middlewares/permission');
 const getReviewById = require('./handler/review/getReviewById');
 const getReviewByPenjualId = require('./handler/review/getReviewByPenjualId');
+const updateReview = require('./handler/review/updateReview');
 const router = express.Router();
 
 router.post('/create', verifyToken, permission('user') ,createReview);
 router.get('/:id', getReviewById);
-router.get('/penjual/:id', getReviewByPenjualId)
+router.get('/penjual/:id', getReviewByPenjualId);
+router.patch('/:id', verifyToken, permission('user'), updateReview);
 
 module.exports = router;
