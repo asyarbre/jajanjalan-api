9a8a9454448c91af484bba0f053d861893d9fd1f
Asya'ri <asyari818@gmail.com>
2023-12-07 14:10:55 +0700

    feat(penjual): add update data

--- src/routes/penjual.js
+++ src/routes/penjual.js
@@ -5,11 +5,13 @@ const createPenjual = require("./handler/penjual/createPenjual");
 const getByPenjualId = require("./handler/penjual/getByPenjualId");
 const deletePenjual = require("./handler/penjual/deletePenjual");
 const getAllPenjual = require("./handler/penjual/getAllPenjual");
+const updatePenjual = require("./handler/penjual/updatePenjual");
 const router = express.Router();
 
 router.get("/", getAllPenjual);
 router.post("/create", verifyToken, permission('penjual'), createPenjual);
 router.get("/:id", getByPenjualId);
+router.patch("/:id", verifyToken, permission('penjual'), updatePenjual);
 router.delete("/:id", verifyToken, permission('penjual'), deletePenjual);
 
 module.exports = router;
